0f454d73aeda307ff6b601cb9a78741b9a8d0b17
Kai Wen Khor <kkho0019@student.monash.edu>
2024-04-27 02:14:18 +1000

    Added sign up function.

--- Kollect-App/Controller/SignUpViewController.swift
+++ Kollect-App/Controller/SignUpViewController.swift
@@ -6,24 +6,66 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var authController = Auth.auth()
+    var authHandle: AuthStateDidChangeListenerHandle?
+    weak var databaseController: DatabaseProtocol?
+    
+    @IBAction func signUp(_ sender: Any) {
+        guard let email = emailTextField.text, let username = usernameTextField.text, let password = passwordTextField.text else {
+            return
+        }
+        
+        if email.isEmpty || username.isEmpty || password.isEmpty {
+            var errorMsg = "Please ensure all fields are filled:"
+            if email.isEmpty {
+                errorMsg += "\n- Must provide an email"
+            }
+            if username.isEmpty {
+                errorMsg += "\n- Must provide a username"
+            }
+            if password.isEmpty {
+                errorMsg += "\n- Must provide a password"
+            }
+            displayMessage(title: "Incomplete Form", message: errorMsg)
+            return
+        }
+        
+        Task {
+            await databaseController?.createAccount(email: email, username: username, password: password)
+            navigationController?.popViewController(animated: false)
+        }
+    }
+    
+    @IBAction func logIn(_ sender: Any) {
+        navigationController?.popViewController(animated: false)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        databaseController = appDelegate?.databaseController
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        authHandle = authController.addStateDidChangeListener { (auth, user) in
+            // Identify user
+            user?.isAnonymous == true ? print("Anonymous user \(String(describing: user?.uid))") : print("User \(String(describing: user?.uid))")
+        }
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        authController.removeStateDidChangeListener(authHandle!)
+    }
 
 }
