3301c6039735729fce6d7c3428bca0d56d976c0f
Kai Wen Khor <kkho0019@student.monash.edu>
2024-04-27 02:21:41 +1000

    Implemented album and user related functions.

--- Kollect-App/Database/FirebaseController.swift
+++ Kollect-App/Database/FirebaseController.swift
@@ -14,12 +14,15 @@ class FirebaseController: NSObject, DatabaseProtocol {
     var listeners = MulticastDelegate<DatabaseListener>()
     var artistList: [Artist]
     var groupList: [Group]
+    var albumList: [Album]
     var photocardList: [Photocard]
     var currentUser: User
+    let DEFAULT_USERNAME = "Anonymous"
     var authController: Auth
     var database: Firestore
     var artistsRef: CollectionReference?
     var groupsRef: CollectionReference?
+    var albumsRef: CollectionReference?
     var photocardsRef: CollectionReference?
     var usersRef: CollectionReference?
     
@@ -30,10 +33,22 @@ class FirebaseController: NSObject, DatabaseProtocol {
         
         artistList = [Artist]()
         groupList = [Group]()
+        albumList = [Album]()
         photocardList = [Photocard]()
         currentUser = User()
         
         super.init()
+        
+        // Sign in anonymously
+        Task {
+            do {
+                let authResult = try await authController.signInAnonymously()
+                currentUser = addUser(userID: authResult.user.uid, username: DEFAULT_USERNAME, isAnonymous: true)
+            } catch {
+                fatalError("Firebase Authentication Failed with Error \(String(describing: error))")
+            }
+            self.setupArtistListener()
+        }
     }
     
     func logInAccount(email: String, password: String) async {
@@ -53,10 +68,10 @@ class FirebaseController: NSObject, DatabaseProtocol {
         self.setupUserListener()
     }
     
-    func createAccount(email: String, password: String) async {
+    func createAccount(email: String, username: String, password: String) async {
         do {
             let authResult = try await authController.createUser(withEmail: email, password: password)
-            currentUser = addUser(userID: authResult.user.uid)
+            currentUser = addUser(userID: authResult.user.uid, username: username, isAnonymous: false)
         } catch {
             print("User creation failed with error: \(error.localizedDescription)")
         }
@@ -86,6 +101,11 @@ class FirebaseController: NSObject, DatabaseProtocol {
             listener.onAllGroupsChange(change: .update, groups: groupList)
         }
         
+        // Album
+        if listener.listenerType == .album || listener.listenerType == .all {
+            listener.onAllAlbumsChange(change: .update, albums: albumList)
+        }
+        
         // Photocard
         if listener.listenerType == .photocard || listener.listenerType == .all {
             listener.onAllPhotocardsChange(change: .update, photocards: photocardList)
@@ -93,7 +113,7 @@ class FirebaseController: NSObject, DatabaseProtocol {
         
         // Collection
         if listener.listenerType == .user || listener.listenerType == .all {
-            listener.onUserChange(change: .update, userCollection: currentUser.all, userFavourites: currentUser.favourites, userWishlist: currentUser.wishlist)
+            listener.onUserChange(change: .update, user: currentUser)
         }
     }
     
@@ -129,7 +149,7 @@ class FirebaseController: NSObject, DatabaseProtocol {
         let group = Group()
         group.name = groupName
         
-        if let groupRef = groupsRef?.addDocument(data: ["name": groupName, "members": []]) {
+        if let groupRef = groupsRef?.addDocument(data: ["name": groupName]) {
             group.id = groupRef.documentID
         }
         
@@ -166,16 +186,88 @@ class FirebaseController: NSObject, DatabaseProtocol {
         }
     }
     
-    func addPhotocard(artist: Artist, group: Group, album: String) -> Photocard {
+    func addAlbum(albumName: String, albumImage: String) -> Album {
+        let album = Album()
+        album.name = albumName
+        album.image = albumImage
+        
+        if let albumRef = albumsRef?.addDocument(data: ["name": albumName, "image": albumImage]) {
+            album.id = albumRef.documentID
+        }
+        
+        return album
+    }
+    
+    func deleteAlbum(album: Album) {
+        if let albumID = album.id {
+            albumsRef?.document(albumID).delete()
+        }
+    }
+    
+    func addAlbumToGroup(album: Album, group: Group) -> Bool {
+        guard let albumID = album.id, let groupID = group.id else {
+            return false
+        }
+        
+        if let newAlbumRef = albumsRef?.document(albumID) {
+            groupsRef?.document(groupID).updateData(
+                ["albums": FieldValue.arrayUnion([newAlbumRef])]
+            )
+        }
+        
+        return true
+    }
+    
+    func removeAlbumFromGroup(album: Album, group: Group) {
+        if group.albums.contains(album), let groupID = group.id, let albumID = album.id {
+            if let removedAlbumRef = albumsRef?.document(albumID) {
+                groupsRef?.document(groupID).updateData(
+                    ["albums": FieldValue.arrayRemove([removedAlbumRef])]
+                )
+            }
+        }
+    }
+    
+    func addPhotocardToAlbum(photocard: Photocard, album: Album) -> Bool {
+        guard let photocardID = photocard.id, let albumID = album.id else {
+            return false
+        }
+        
+        if let newPhotocardRef = photocardsRef?.document(photocardID) {
+            albumsRef?.document(albumID).updateData(
+                ["photocards": FieldValue.arrayUnion([newPhotocardRef])]
+            )
+        }
+        
+        return true
+    }
+    
+    func removePhotocardFromAlbum(photocard: Photocard, album: Album) {
+        if album.photocards.contains(photocard), let albumID = album.id, let photocardID = photocard.id {
+            if let removedPhotocardRef = photocardsRef?.document(albumID) {
+                albumsRef?.document(albumID).updateData(
+                    ["albums": FieldValue.arrayRemove([removedPhotocardRef])]
+                )
+            }
+        }
+    }
+    
+    func addPhotocard(artist: Artist, group: Group, album: Album, image: String) -> Photocard {
         let photocard = Photocard()
         photocard.artist = artist
         photocard.group = group
         photocard.album = album
+        photocard.image = image
         
         // Should save artist name only or artist object
-        if let photocardRef = photocardsRef?.addDocument(data: ["artist": artist.name!, "group": group.name!, "album": album]) {
+        if let photocardRef = photocardsRef?.addDocument(data: ["artist": artist, "group": group, "album": album, "image": image]) {
             
             photocard.id = photocardRef.documentID
+        }
+        
+        // Add to Group -> Album -> Artist
+        if !addPhotocardToAlbum(photocard: photocard, album: album) {
+            print("Error adding photocard to album.")
         }
         
         return photocard
@@ -187,14 +279,16 @@ class FirebaseController: NSObject, DatabaseProtocol {
         }
     }
     
-    func addUser(userID: String) -> User {
+    func addUser(userID: String, username: String, isAnonymous: Bool) -> User {
         usersRef = database.collection("users")
         let user = User()
         user.id = userID
+        user.name = username
+        user.isAnonymous = isAnonymous
         
         // Create user document
         if let userRef = usersRef?.document(userID) {
-            userRef.setData(["name": userID, "all": [], "favourites": [], "wishlist": []])
+            userRef.setData(["name": username])
         }
         
         return user
@@ -463,9 +557,56 @@ class FirebaseController: NSObject, DatabaseProtocol {
         
         listeners.invoke { (listener) in
             if listener.listenerType == ListenerType.user {
-                listener.onUserChange(change: .update, userCollection: currentUser.all, userFavourites: currentUser.favourites, userWishlist: currentUser.wishlist)
+                listener.onUserChange(change: .update, user: currentUser)
             }
         }
     }
     
+    // Only call when adding initial data
+    func createDefaults() {
+        let artistA = addArtist(artistName: "A", artistBirthday: "2001-01-01")
+        let artistB = addArtist(artistName: "B", artistBirthday: "2002-02-02")
+        let artistC = addArtist(artistName: "C", artistBirthday: "2003-03-03")
+        let artistD = addArtist(artistName: "D", artistBirthday: "2004-04-04")
+        let artistE = addArtist(artistName: "E", artistBirthday: "2005-05-05")
+        let artistF = addArtist(artistName: "F", artistBirthday: "2006-06-06")
+        
+        let groupA = addGroup(groupName: "123")
+        let groupB = addGroup(groupName: "456")
+        let groupC = addGroup(groupName: "789")
+        
+        let albumA = addAlbum(albumName: "Album 1", albumImage: "")
+        let albumB = addAlbum(albumName: "Album 2", albumImage: "")
+        let albumC = addAlbum(albumName: "Album 3", albumImage: "")
+        let albumD = addAlbum(albumName: "Album 4", albumImage: "")
+        let albumE = addAlbum(albumName: "Album 5", albumImage: "")
+        let albumF = addAlbum(albumName: "Album 6", albumImage: "")
+        
+        let _ = addArtistToGroup(artist: artistA, group: groupA)
+        let _ = addArtistToGroup(artist: artistB, group: groupA)
+        let _ = addArtistToGroup(artist: artistC, group: groupB)
+        let _ = addArtistToGroup(artist: artistD, group: groupB)
+        let _ = addArtistToGroup(artist: artistE, group: groupC)
+        let _ = addArtistToGroup(artist: artistF, group: groupC)
+        
+        let _ = addAlbumToGroup(album: albumA, group: groupA)
+        let _ = addAlbumToGroup(album: albumB, group: groupA)
+        let _ = addAlbumToGroup(album: albumC, group: groupB)
+        let _ = addAlbumToGroup(album: albumD, group: groupB)
+        let _ = addAlbumToGroup(album: albumE, group: groupC)
+        let _ = addAlbumToGroup(album: albumF, group: groupC)
+        
+        let _ = addPhotocard(artist: artistA, group: groupA, album: albumA, image: "")
+        let _ = addPhotocard(artist: artistA, group: groupA, album: albumB, image: "")
+        let _ = addPhotocard(artist: artistB, group: groupA, album: albumA, image: "")
+        let _ = addPhotocard(artist: artistB, group: groupA, album: albumB, image: "")
+        let _ = addPhotocard(artist: artistC, group: groupB, album: albumC, image: "")
+        let _ = addPhotocard(artist: artistC, group: groupB, album: albumD, image: "")
+        let _ = addPhotocard(artist: artistD, group: groupB, album: albumC, image: "")
+        let _ = addPhotocard(artist: artistD, group: groupB, album: albumD, image: "")
+        let _ = addPhotocard(artist: artistE, group: groupC, album: albumE, image: "")
+        let _ = addPhotocard(artist: artistE, group: groupC, album: albumF, image: "")
+        let _ = addPhotocard(artist: artistF, group: groupC, album: albumE, image: "")
+        let _ = addPhotocard(artist: artistF, group: groupC, album: albumF, image: "")
+    }
 }
