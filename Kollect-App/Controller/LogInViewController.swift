61ddfaacfc201388206da062619f89d432df92b2
Kai Wen Khor <kkho0019@student.monash.edu>
2024-04-27 02:19:33 +1000

    Implemented log in and sign up functions.

--- Kollect-App/Controller/LogInViewController.swift
+++ Kollect-App/Controller/LogInViewController.swift
@@ -6,24 +6,64 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LogInViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var authController = Auth.auth()
+    var authHandle: AuthStateDidChangeListenerHandle?
+    weak var databaseController: DatabaseProtocol?
+    
+    @IBAction func logIn(_ sender: Any) {
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+            return
+        }
+        
+        if email.isEmpty || password.isEmpty {
+            var errorMsg = "Please ensure all fields are filled:"
+            if email.isEmpty {
+                errorMsg += "\n- Must provide an email"
+            }
+            if password.isEmpty {
+                errorMsg += "\n- Must provide a password"
+            }
+            displayMessage(title: "Incomplete Form", message: errorMsg)
+            return
+        }
+        
+        Task {
+            await databaseController?.logInAccount(email: email, password: password)
+            navigationController?.popViewController(animated: true)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUp(_ sender: Any) {
+        performSegue(withIdentifier: "signUpFromMenuSegue", sender: self)
+        emailTextField.text?.removeAll()
+        passwordTextField.text?.removeAll()
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        databaseController = appDelegate?.databaseController
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        authHandle = authController.addStateDidChangeListener { (auth, user) in
+            // Identify user
+            user?.isAnonymous == true ? print("Anonymous user \(String(describing: user?.uid))") : print("User \(String(describing: user?.uid))")
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        authController.removeStateDidChangeListener(authHandle!)
+    }
 
 }
