5ebcc076d12c06fdc02f1560a764ac3dd28eabad
Kai Wen Khor <kkho0019@student.monash.edu>
2024-04-27 02:07:51 +1000

    Added Album class.

---
+++ Kollect-App/Model/Album.swift
@@ -0,0 +1,18 @@
+//
+//  Album.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 26/04/2024.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+class Album: NSObject, Codable {
+    @DocumentID var id: String?
+    var name: String?
+    var image: String?
+    var photocards = [Photocard]()
+    // Future: Sort by date
+//    var dateReleased: Date?
+}
