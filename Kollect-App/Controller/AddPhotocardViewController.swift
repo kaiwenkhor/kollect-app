1b34af5bcf681ded46d65d1091b585d8346b80f1
Kai Wen Khor <kkho0019@student.monash.edu>
2024-04-29 22:36:04 +1000

    Modified addPhotocard to store document references as fields.

---
+++ Kollect-App/Controller/AddPhotocardViewController.swift
@@ -0,0 +1,63 @@
+//
+//  AddPhotocardViewController.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 28/04/2024.
+//
+
+import UIKit
+
+class AddPhotocardViewController: UIViewController {
+
+    @IBOutlet weak var artistTextField: UITextField!
+    @IBOutlet weak var albumTextField: UITextField!
+    @IBOutlet weak var memberTextField: UITextField!
+    
+    var listenerType: ListenerType = .user
+    weak var databaseController: DatabaseProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        databaseController = appDelegate?.databaseController
+        
+        // Let text field select Objects (Artist, Album, Idol)
+    }
+    
+    @IBAction func findPhotocard(_ sender: Any) {
+        guard let artist = artistTextField.text, let album = albumTextField.text, let member = memberTextField.text else {
+            return
+        }
+        
+        if
+        if artist.isEmpty || album.isEmpty {
+            var errorMsg = "Please ensure all fields are filled:"
+            if artist.isEmpty {
+                errorMsg += "\n- Must provide an email"
+            }
+            if album.isEmpty {
+                errorMsg += "\n- Must provide a password"
+            }
+            displayMessage(title: "Incomplete Form", message: errorMsg)
+            return
+        }
+        
+        Task {
+            await databaseController?.logInAccount(email: email, password: password)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
