8188fc1687be215192626f29d56816ea4ae88b97
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-04 02:12:58 +1000

    Added reusable cell for photocard collection view.

---
+++ Kollect-App/Controller/Collection/AllPhotocardsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AllPhotocardsCollectionViewCell.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 03/05/2024.
+//
+
+import UIKit
+
+class AllPhotocardsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photocardImageView: UIImageView!
+}
