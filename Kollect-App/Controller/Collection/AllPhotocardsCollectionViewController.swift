eb5a4245fc0363fd19a1e9f2692c764925beb11f
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-04 02:13:45 +1000

    Added and implemented functions in select photocards collection view controller.

---
+++ Kollect-App/Controller/Collection/AllPhotocardsCollectionViewController.swift
@@ -0,0 +1,126 @@
+//
+//  AllPhotocardsCollectionViewController.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 03/05/2024.
+//
+
+import UIKit
+
+class AllPhotocardsCollectionViewController: UICollectionViewController, UISearchResultsUpdating {
+    
+    let CELL_PHOTOCARD = "photocardCell"
+    var allPhotocards = [Photocard]()
+    var filteredPhotocards = [Photocard]()
+    weak var databaseController: DatabaseProtocol?
+    let DEFAULT_IMAGE = "Default_Photocard_Image"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Select Photocard"
+        navigationItem.backButtonTitle = "Back"
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        databaseController = appDelegate?.databaseController
+        
+        filteredPhotocards = allPhotocards
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search All Photocards"
+        navigationItem.searchController = searchController
+        
+        // This view controller decides how the search controller is presented.
+        definesPresentationContext = true
+        
+        collectionView.setCollectionViewLayout(createLayout(), animated: false)
+    }
+    
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        /*
+         * Tiled layout
+         * - Group is three photocards, side-by-side
+         * - Group width is 1 x screen width, and height is 17/33 screen width (photocard height)
+         * - Poster width is 1/3 x group width, with height as 1 x group width
+         * - This makes item dimensions 5.5:8.5
+         * - contentInsets puts a 1 pixel margin around each photocard
+         */
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3), heightDimension: .fractionalHeight(1))
+        let itemLayout = NSCollectionLayoutItem(layoutSize: itemSize)
+        itemLayout.contentInsets = NSDirectionalEdgeInsets(top: 1, leading: 1, bottom: 1, trailing: 1)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(17/33))
+        let groupLayout = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [itemLayout])
+        
+        let sectionLayout = NSCollectionLayoutSection(group: groupLayout)
+        
+        return UICollectionViewCompositionalLayout(section: sectionLayout)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Download photocard images?
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filteredPhotocards.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CELL_PHOTOCARD, for: indexPath) as! AllPhotocardsCollectionViewCell
+    
+        let photocard = filteredPhotocards[indexPath.row]
+        cell.photocardImageView.image = UIImage(named: photocard.image ?? DEFAULT_IMAGE)
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let photocard = filteredPhotocards[indexPath.row]
+        print("PHOTOCARD \(photocard.image ?? DEFAULT_IMAGE) SELECTED")
+    }
+    
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else {
+            return
+        }
+        
+        if searchText.count > 0 {
+            filteredPhotocards = allPhotocards.filter({ (photocard: Photocard) -> Bool in
+                let searchArtist = photocard.artist?.name?.lowercased().contains(searchText) ?? false
+                let searchIdol = photocard.idol?.name?.lowercased().contains(searchText) ?? false
+                let searchAlbum =  photocard.album?.name?.lowercased().contains(searchText) ?? false
+                return searchArtist || searchIdol || searchAlbum
+            })
+        } else {
+            filteredPhotocards = allPhotocards
+        }
+        
+        collectionView.reloadData()
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //
+    }
+
+}
