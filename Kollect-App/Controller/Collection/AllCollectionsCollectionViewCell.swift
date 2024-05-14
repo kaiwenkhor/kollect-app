ba9c31fd75b1a2e18b13893941932843c0e13bcc
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-14 23:03:51 +1000

    Added AllCollectionsCollectionViewCell.

---
+++ Kollect-App/Controller/Collection/AllCollectionsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AllCollectionsCollectionViewCell.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 06/05/2024.
+//
+
+import UIKit
+
+class AllCollectionsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photocardImageView: UIImageView!
+    @IBOutlet weak var favouriteImageView: UIImageView!
+}
