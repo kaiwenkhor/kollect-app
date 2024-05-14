7ab1777a43f661b9069b52e0a50471372a86bd94
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-14 22:59:26 +1000

    Added FavouritesCollectionViewCell.

---
+++ Kollect-App/Controller/Collection/FavouritesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AddToFavouritesCollectionViewCell.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 08/05/2024.
+//
+
+import UIKit
+
+class FavouritesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photocardImageView: UIImageView!
+    @IBOutlet weak var favouriteImageView: UIImageView!
+}
