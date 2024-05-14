f690d2b1f4a68598646765d7d2f52f9961180d83
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-14 22:56:16 +1000

    Added delegate to set collection header button action.

---
+++ Kollect-App/SetHeaderButtonActionDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderCollectionReusableViewDelegate.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 14/05/2024.
+//
+
+import Foundation
+
+protocol SetHeaderButtonActionDelegate: AnyObject {
+    func showPhotocards(section: Int)
+}
