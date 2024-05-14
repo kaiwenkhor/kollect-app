fe5feaa51ed8dafd3ded4eb1b10a825d37447d4c
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-14 23:05:00 +1000

    Added AddToWishlistCollectionViewCell.

---
+++ Kollect-App/Controller/Collection/AddToWishlistCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AddToWishlistCollectionViewCell.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 08/05/2024.
+//
+
+import UIKit
+
+class AddToWishlistCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photocardImageView: UIImageView!
+}
