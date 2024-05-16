4efd4b069618d101ae6d801222bdc75d934ad041
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-16 16:02:28 +1000

    Added a user table view cell.

---
+++ Kollect-App/Controller/Menu/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 15/05/2024.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
