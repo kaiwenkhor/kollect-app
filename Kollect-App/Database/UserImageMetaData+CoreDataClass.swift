9762ab37a82bac0842d8654f0cacd9f79a37b494
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-23 23:04:31 +1000

    Added data model.

---
+++ Kollect-App/Database/UserImageMetaData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserImageMetaData+CoreDataClass.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 19/05/2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserImageMetaData)
+public class UserImageMetaData: NSManagedObject {
+
+}
