e9f08fca2c3dc44146a8b83269e135b083649355
Kai Wen Khor <kkho0019@student.monash.edu>
2024-05-23 23:10:30 +1000

    Deleted file.

---
+++ Kollect-App/Controller/Menu/WishlistCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WishlistCollectionViewCell.swift
+//  Kollect-App
+//
+//  Created by Daryl Khor on 26/04/2024.
+//
+
+import UIKit
+
+class WishlistCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photocardImageView: UIImageView!
+}
